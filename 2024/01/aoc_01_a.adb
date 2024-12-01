fa8c6724c57ed8117af9882ae09bb6a3d241ebc4
RREE <rolf.ebert.gcc@gmx.de>
2024-12-01 20:11:29 +0100

    a new start. AoC 2024

---
+++ 2024/01/aoc_01_a.adb
@@ -0,0 +1,20 @@
+with Ada.Text_IO;             use Ada.Text_IO;
+with Ada.Integer_Text_IO;     use Ada.Integer_Text_IO;
+with Aoc_Helper;              use Aoc_Helper;
+
+procedure AoC_01_A is
+
+begin
+   Open_Input;
+
+   while not End_Of_File (Input) loop
+      declare
+         Line : String := Get_Line(Input);
+      begin
+         null;
+      end;
+   end loop;
+
+   Put_Line ("Result: ");
+
+end AoC_01_A;
